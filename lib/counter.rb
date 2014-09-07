7f67ccbc519c6fa2419ded7e845bbf671ec1a932
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-09-07 13:19:53 +0100

    It can read the last line of a file

--- lib/counter.rb
+++ lib/counter.rb
@@ -4,3 +4,8 @@ def first_line(file_path)
 	lines = IO.readlines(file_path)
 	lines.first.to_i
 end
+
+def last_line(file_path)
+	lines = IO.readlines(file_path)
+	lines.last.to_i
+end
--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -6,4 +6,8 @@ describe 'Palindrome counter' do
 		expect(first_line('test_files/example.txt')).to eq 1
 	end
 
+	it "can read the last line of a file" do
+		expect(last_line('test_files/example.txt')).to eq 100
+	end
+
 end
